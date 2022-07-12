e46b0d7d4af5816eeb7ccf281e6da76c33ea09e5
raviteja628 <raviteja628@gmail.com>
2022-07-12 08:51:17 +0530

    added new cloud function

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import os
 from google.cloud import bigquery
-
-def csv_loader(data, context):
+def csv_loader_ravi_ravi(data, context):
         client = bigquery.Client()
         dataset_id = os.environ['DATASET']
         dataset_ref = client.dataset(dataset_id)
@@ -16,22 +15,17 @@ def csv_loader(data, context):
                 ]
         job_config.skip_leading_rows = 1
         job_config.source_format = bigquery.SourceFormat.CSV
-
         # get the URI for uploaded CSV in GCS from 'data'
         uri = 'gs://' + os.environ['BUCKET'] + '/' + data['name']
-
         # lets do this
         load_job = client.load_table_from_uri(
                 uri,
                 dataset_ref.table(os.environ['TABLE']),
                 job_config=job_config)
-
         print('Starting job {}'.format(load_job.job_id))
-        print('Function=csv_loader, Version=' + os.environ['VERSION'])
+        print('Function=csv_loader_ravi, Version=' + os.environ['VERSION'])
         print('File: {}'.format(data['name']))
-
         load_job.result()  # wait for table load to complete.
         print('Job finished.')
-
         destination_table = client.get_table(dataset_ref.table(os.environ['TABLE']))
         print('Loaded {} rows.'.format(destination_table.num_rows))
